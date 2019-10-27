4e29c1d2e48a62fb1d64248827fc4fe84bfa436d
Rayjay-8 <rayarruda9876@gmail.com>
2019-10-26 21:06:47 -0300

    Update pastaSpoofName.py

--- pastaSpoofName.py
+++ pastaSpoofName.py
@@ -2,6 +2,18 @@ import os,sys
 
 def spoof(sp):
         name = sp.split(".")[0]
-        newname = os.getcwd()+os.sep+name+u"\u202E"
-        os.mkdir(newname)
-spoof("  ")
+        path = sp.split(".")[1]
+        print(sp)
+        print(name)
+        print(path)
+        
+        #newname = os.getcwd()+os.sep+name+u"\u202E"
+        newname = name+u"\u202E"+"TXT."+path
+        print(newname)
+        #os.mkdir(newname)
+        with open(newname,"w") as tk:
+                tk.write("")
+#spoof("teste.exe")
+ffile = 'tetes.lnk'
+print(os.listdir("."))
+os.rename(ffile,"teste.lnk"+u"\u202E"+" TXT. ")
