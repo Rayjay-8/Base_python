445577e91137c28c9889129fc334f21c848acc8e
Rayjay-8 <rayarruda9876@gmail.com>
2020-01-11 22:57:46 -0300

    Update Get saldo chip dos correios.py

--- Get saldo chip dos correios.py
+++ Get saldo chip dos correios.py
@@ -1,7 +1,7 @@
 import requests,json,time
 
 #url2 = "https://apps.correios.com.br/correioscelular/v1/clientes/plataforma/A/versao/1.1.1"
-#                                                                                           /NumeroCorreto
+#                                                             /NumeroCorreto
 url = "https://apps.correios.com.br/correioscelular/v1/saldos/44920005176"
 auth = "Basic MTU4OTE6YXBwMjAxOA=="
 UserAgente = "Dalvik/2.1.0 (Linux; U; Android 6.0.1; SM-G532MT Build/MMB29T)"
